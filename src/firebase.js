78748362db79f674beb86dfe6e33daf3bbd0f8f1
federicofrache <federicofrache@gmail.com>
2026-07-28 18:23:12 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage";
 //    -> creá una "App web" (</>) si no tenés una, y copiá el objeto firebaseConfig de ahí.
 // 2) Reemplazá los valores de abajo por los tuyos.
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID",
+  apiKey: "AIzaSyD5ThZCKkeFGORCEot1P4YulLzluWedkX4",
+  authDomain: "margenes-cultivos.firebaseapp.com",
+  projectId: "margenes-cultivos",
+  storageBucket: "margenes-cultivos.firebasestorage.app",
+  messagingSenderId: "720906475603",
+  appId: "1:720906475603:web:a160edb66f1931f1b14ea6",
 };
 
 const app = initializeApp(firebaseConfig);
